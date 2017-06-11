512069536781b0e6bd6baa147ea7e23103df305d
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2017-06-12 01:05:14 +0200

    fix new line vscode issue

--- meson/post_install.py
+++ meson/post_install.py
@@ -3,8 +3,7 @@
 import os
 import subprocess
 
-schemadir = os.path.join(
-    os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
